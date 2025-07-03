b26778b57406499c4332e819e552dc69cbcdf252
Amreen78612 <amreensm6264@gmail.com>
2025-07-03 23:58:38 +0530

    deploye

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from "./Home";
+import "./index.css";
 
 function App() {
-  const [name, setName] = useState("");
-/*  const apiUrl = process.env.VITE_BACKEND_URL;
- console.log(apiUrl); */
+  
 
-  const handleGenerate = async (e) => {
-    e.preventDefault();
-    if (!name) {
-      alert("Please enter your name");
-      return;
-    }
 
-    const response = await fetch(`https://backend-certificate-ygqz.onrender.com/generate-certificate`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name }),
-    });
-
-    const blob = await response.blob();
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = `${name}-certificate.pdf`;
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-  };
-
-    return (
-        <div style={{ textAlign: 'center', marginTop: '100px' }}>
-            <h1> 🎓 Certificate Generator
-        </h1>
-        <p className="text-gray-600 mb-6 text-sm sm:text-base px-2 sm:px-6">
-          Enter your name below to generate your custom certificate.
-        </p>
-            <form onSubmit={handleGenerate}>
-                <input
-                    type="text"
-                    placeholder="Enter your name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    style={{ padding: '10px', width: '300px' }}
-                />
-                <br /><br />
-                <button type="submit" style={{ padding: '10px 20px' }}>
-                    Generate Certificate
-                </button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </header>
+    </div>
+  );
 }
 
 export default App;
